11c2f91f7f45fe2de5f49d7b671f471e1edf25aa
seraphicblue <ggj05037@gmail.com>
2024-09-04 15:07:22 +0900

    fix: Correct incorrect imports and service injections in ProductModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,31 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './product.entity';
+import { Product } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductRepository } from './repositories/product.repository';
 import { ProductCreatedHandler } from './events/handlers/product-created.handler';
 import { CreateProductHandler } from './commands/handlers/create-product.handler';
 import { ProductView, ProductViewSchema } from './schemas/product-view.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { EventStoreModule } from 'src/shared/event-store/event-store.module';
-import { CommandHandler, EventsHandler } from '@nestjs/cqrs';
+import { EventStoreModule } from '../shared/event-store/event-store.module';
+import { CommandHandler, CqrsModule, EventsHandler } from '@nestjs/cqrs';
+import { ProductViewRepository } from './repositories/product-view.repository';
 
 
-export const CommandHandlers = [ProductCreatedHandler]
-export const EventsHandlers = [CreateProductHandler]
+const CommandHandlers = [CreateProductHandler]
+const EventsHandlers = [ProductCreatedHandler]
+
 @Module({
   imports: [
+    CqrsModule,
     MongooseModule.forFeature([{ name: ProductView.name, schema: ProductViewSchema }]),
     TypeOrmModule.forFeature([Product]),
     EventStoreModule,
   ],
-  providers: [ProductRepository,
+  providers: [
     ...CommandHandlers,
     ...EventsHandlers,
-    ProductView,
-    ProductRepository
+    ProductRepository,
+    ProductViewRepository
   ],
   controllers: [ProductController],
-  exports: [TypeOrmModule],
+  exports: [ProductRepository],
+  
 })
 export class ProductModule { }
