a3cfc28fa073c3eb7c0da2228eac38a6c3da3aee
boogie-bogie <boogie.bogiegie@gmail.com>
2024-09-07 03:05:58 +0900

    Add: new features - user-activities

---
+++ src/user-activities/repositories/access-log.repository.ts
@@ -0,0 +1,18 @@
+import { Repository } from 'typeorm';
+import { AccessLog } from '../entities/access-log.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class AccessLogRepository {
+
+  constructor(
+      @InjectRepository(AccessLog)
+      private readonly accessLogRepository: Repository<AccessLog>
+  ){}
+
+  async createLog(logData: Partial<AccessLog>): Promise<AccessLog> {
+    const log = this.accessLogRepository.create(logData);
+    return this.accessLogRepository.save(log);
+  }
+}
---
+++ src/user-activities/services/access-log.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AccessLogRepository } from '../repositories/access-log.repository';
+import { AccessLog } from '../entities/access-log.entity';
+
+@Injectable()
+export class AccessLogService {
+  constructor(
+    @InjectRepository(AccessLogRepository)
+    private readonly accessLogRepository: AccessLogRepository
+  ) {}
+
+  async logUserAction(logData: Partial<AccessLog>): Promise<AccessLog> {
+    return this.accessLogRepository.createLog(logData);
+  }
+}
