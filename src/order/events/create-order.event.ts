fc99762b279ee0809eaac6924cc0a62195c95806
jcowwk <jcowwk@gmail.com>
2024-09-11 18:44:03 +0900

    fix: changing wrong words

--- src/order/events/create-order.event.ts
+++ src/order/events/create-order.event.ts
@@ -1,13 +1,11 @@
-import { PaymentMethod, Status } from "../enums/order.enum";
-
 export class CreateOrderEvent {
     constructor(
         public readonly userId: number,
         public readonly sellerId: number,
         public readonly totalAmount: number,
         public readonly pickupTime: Date,
-        public readonly paymentMethod: PaymentMethod,
-        public readonly status: Status,
+        public readonly paymentMethod: string,
+        public readonly status: string,
         public readonly items: any,
         public readonly createdAt: Date = new Date(),
         public readonly updatedAt: Date = new Date()
