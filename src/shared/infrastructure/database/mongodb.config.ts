355ead8b1c1e17b707f24e425f43d5c2014c06d9
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-09-19 01:35:17 +0900

    Fix: DB config files-03

--- src/shared/infrastructure/database/mongodb.config.ts
+++ src/shared/infrastructure/database/mongodb.config.ts
@@ -7,25 +7,38 @@
 //   uri: configService.get<string>("MONGODB_URI"),
 // });
 
-import { MongooseModuleOptions } from "@nestjs/mongoose";
+// import { MongooseModuleOptions } from "@nestjs/mongoose";
+// import { ConfigService } from "@nestjs/config";
+// import * as fs from "fs";
+// import * as path from "path";
+
+// export const getMongoConfig = (
+//   configService: ConfigService,
+// ): MongooseModuleOptions => {
+//   const uri = configService.get<string>("MONGODB_URI");
+
+//   if (!uri) {
+//     throw new Error("MONGODB_URI is not defined in the environment variables");
+//   }
+
+//   // 인증서 파일 경로 설정
+//   const tlsCAFilePath = path.resolve("/home/ubuntu/app", "global-bundle.pem");
+
+//   return {
+//     uri,
+//     tlsCAFile: fs.readFileSync(tlsCAFilePath, "utf-8"), // 'utf-8'을 지정하여 string으로 변환
+//   };
+// };
+
 import { ConfigService } from "@nestjs/config";
 import * as fs from "fs";
-import * as path from "path";
 
-export const getMongoConfig = (
-  configService: ConfigService,
-): MongooseModuleOptions => {
-  const uri = configService.get<string>("MONGODB_URI");
-
-  if (!uri) {
-    throw new Error("MONGODB_URI is not defined in the environment variables");
-  }
-
-  // 인증서 파일 경로 설정
-  const tlsCAFilePath = path.resolve("/home/ubuntu/app", "global-bundle.pem");
-
+export const getMongoConfig = (configService: ConfigService) => {
+  const caFilePath = configService.get<string>("MONGO_TLS_CA_FILE_PATH");
+  console.log("Using CA file path:", caFilePath);
   return {
-    uri,
-    tlsCAFile: fs.readFileSync(tlsCAFilePath, "utf-8"), // 'utf-8'을 지정하여 string으로 변환
+    uri: configService.get<string>("MONGO_URI"),
+    tls: true,
+    tlsCAFile: caFilePath ? fs.readFileSync(caFilePath, "utf-8") : null,
   };
 };
