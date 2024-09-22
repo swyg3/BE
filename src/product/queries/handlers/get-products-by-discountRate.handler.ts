a7b1238f5314c16f4d3b3e134ee0c0e0f6a78285
seraphicblue <ggj05037@gmail.com>
2024-09-22 15:38:49 +0900

    ready for pull dev

--- src/product/queries/handlers/get-products-by-discountRate.handler.ts
+++ src/product/queries/handlers/get-products-by-discountRate.handler.ts
@@ -17,36 +17,16 @@ export class GetProductByDiscountRateHandler
   async execute(query: GetProductByDiscountRateDto) {
     const { where__id_more_than, take = 5 } = query;
 
-    // 정렬 기준 필드와 마지막으로 반환된 문서의 정렬 기준 값
-    const sortField = 'discountRate';
-    let lastDiscountRate: number | undefined;
-
-    if (where__id_more_than) {
-      const lastProduct = await this.productViewModel.findById(where__id_more_than);
-      if (lastProduct) {
-        lastDiscountRate = lastProduct.discountRate;
-      }
-    }
-
     // 쿼리 조건 설정
-    let queryCondition: any = {};
-    if (lastDiscountRate !== undefined) {
-      queryCondition = {
-        $or: [
-          { [sortField]: { $lt: lastDiscountRate } },
-          { 
-            [sortField]: lastDiscountRate,
-            _id: { $gt: where__id_more_than }
-          }
-        ]
-      };
-    }
+    const queryCondition = where__id_more_than
+      ? { _id: { $gt: where__id_more_than } }
+      : {};
 
     // 데이터베이스에서 정렬 및 페이지네이션 적용
     const products = await this.productViewModel
       .find(queryCondition)
-      .sort({ [sortField]: -1, _id: 1 })
-      .limit(take + 1)
+      .sort({ discountRate: -1 })
+      .limit(take + 1)  // 다음 페이지 확인을 위해 1개 더 가져옴
       .exec();
 
     const hasNextPage = products.length > take;
@@ -56,7 +36,7 @@ export class GetProductByDiscountRateHandler
 
     // 다음 페이지 URL 생성
     let nextUrl: string | null = null;
-    if (hasNextPage && last) {
+    if (hasNextPage) {
       const nextPageQuery = {
         where__id_more_than: last._id.toString(),
         take: take.toString(),
