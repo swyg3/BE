7177b6d04917f682462efffe13b7057158d50e6e
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-09-25 17:09:39 +0900

    Remove: Oauth Login legacy codes removed

--- src/auth/commands/commands/login-oauth.command.ts
+++
@@ -1,9 +0,0 @@
-import { LoginOAuthDto } from "../../dtos/login-oauth.dto";
-
-export class LoginOAuthCommand {
-  constructor(
-    public readonly provider: string,
-    public readonly oneTimeToken: string,
-    public readonly userType: string,
-  ) {}
-}
