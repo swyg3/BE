f5b3be8926af5647aa8110f62c10e8c35435d6b2
jcowwk <jcowwk@gmail.com>
2024-09-26 00:29:16 +0900

    Add: adding GetOrderItemQuery

---
+++ src/order-itmes/queries/handlers/get-order-item.query-handler.ts
@@ -0,0 +1,17 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { InjectRepository } from "@nestjs/typeorm";
+import { OrderItems } from "src/order-itmes/entities/order-items.entity";
+import { Repository } from "typeorm";
+import { GetOrderItemQuery } from "../get-order-item.query";
+
+@QueryHandler(GetOrderItemQuery)
+export class GetOrderItemQueryHandler implements IQueryHandler<GetOrderItemQuery> {
+    constructor(
+        @InjectRepository(OrderItems)
+        private readonly orderItemRepository: Repository<OrderItems>
+    ) {}
+
+    async execute(query: GetOrderItemQuery): Promise<any> {
+        return this.orderItemRepository.find({ where: { orderId: query.orderId }});
+    }
+}
