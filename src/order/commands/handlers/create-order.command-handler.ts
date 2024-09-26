bcdfc52f78c94911933c0c78560e0ef9b592a2d0
jcowwk <jcowwk@gmail.com>
2024-09-26 21:36:50 +0900

    Add: create order event publish

--- src/order/commands/handlers/create-order.command-handler.ts
+++ src/order/commands/handlers/create-order.command-handler.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { CommandHandler, EventBus, ICommandHandler } from "@nestjs/cqrs";
 import { InjectRepository } from "@nestjs/typeorm";
 import { OrderItems } from "src/order-itmes/entities/order-items.entity";
+import { CreateOrderAggregate } from "src/order/aggregates/create-order.aggregate";
 import { Order } from "src/order/entities/order.entity";
 import { Repository } from 'typeorm';
 import { CreateOrderCommand } from "../create-order.command";
@@ -16,6 +17,7 @@ export class CreateOrderCommandHandler implements ICommandHandler<CreateOrderCom
         private readonly orderRepository: Repository<Order>,
         @InjectRepository(OrderItems)
         private readonly orderItemsRepository: Repository<OrderItems>,
+        private readonly eventBus: EventBus,
     ) {}
 
     async execute(command: CreateOrderCommand): Promise<any> {
@@ -47,5 +49,30 @@ export class CreateOrderCommandHandler implements ICommandHandler<CreateOrderCom
         this.logger.log(`주문 내역 및 주문 상세 내역 생성 완료: ${savedOrder.id}`);
 
         // 3. 주문 수량 만큼 재고 삭제
+
+        // 4. 이벤트 aggregate 생성
+        const orderAggregate = new CreateOrderAggregate(savedOrder.id);
+
+        // 5. aggregate에서 주문 등록 이벤트 생성
+        const event = orderAggregate.register(
+            savedOrder.id,
+            userId,
+            totalAmount,
+            totalPrice,
+            newOrder.paymentMethod,
+            newOrder.status,
+            items.map(item => ({
+                orderId: savedOrder.id,
+                productId: item.productId,
+                quantity: item.quantity,
+                price: item.price,
+            })),
+            pickupTime,
+            newOrder.createdAt,
+            new Date(),
+        );
+
+        // 6. 이벤트 발행
+        this.eventBus.publish(event);
     }
 }
