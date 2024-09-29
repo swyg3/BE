b43e80f9928564fab89c1571c171733bea929367
jcowwk <jcowwk@gmail.com>
2024-09-29 16:12:16 +0900

    refactor: get query

--- src/order-itmes/queries/handlers/get-order-item.query-handler.ts
+++ src/order-itmes/queries/handlers/get-order-item.query-handler.ts
@@ -1,29 +1,20 @@
-import { Inject } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
-import { DynamoDB } from "aws-sdk"; // AWS SDK에서 DynamoDB 사용
-import { OrderItemsViewModel } from "src/order-itmes/schemas/order-items-view.schema";
+import { InjectModel, Model } from "nestjs-dynamoose";
+import { OrderItemsView } from "src/order/repositories/order.repository";
 import { GetOrderItemQuery } from "../get-order-item.query";
 
 @QueryHandler(GetOrderItemQuery)
 export class GetOrderItemQueryHandler implements IQueryHandler<GetOrderItemQuery> {
-    private readonly dynamoDB: DynamoDB.DocumentClient;
-
     constructor(
-        @Inject(DynamoDB) private readonly dynamoDBClient: DynamoDB.DocumentClient
-    ) {
-        this.dynamoDB = dynamoDBClient;
-    }
+        @InjectModel("OrderItemsView")
+        private readonly orderItemsViewModel: Model<OrderItemsView, {}> // Dynamoose 모델 주입
+    ) {}
 
-    async execute(query: GetOrderItemQuery): Promise<typeof OrderItemsViewModel[]> {
-        const params = {
-            TableName: "OrderItemsView", // DynamoDB의 테이블 이름
-            FilterExpression: "orderId = :orderId", // orderId 필터
-            ExpressionAttributeValues: {
-                ":orderId": query.orderId, // 쿼리에서 받은 orderId
-            },
-        };
+    async execute(query: GetOrderItemQuery): Promise<OrderItemsView[]> {
+        const result = await this.orderItemsViewModel.query("orderId")
+            .eq(query.orderId) // orderId에 해당하는 항목을 쿼리
+            .exec(); // Dynamoose 쿼리 실행
 
-        const result = await this.dynamoDB.scan(params).promise(); // 모든 주문 항목을 스캔
-        return (result.Items || []) as typeof OrderItemsViewModel[]; // 반환된 아이템을 OrderItemsViewModel 타입으로 변환하여 반환
+        return result; // 결과 반환
     }
 }
--- src/order/queries/handlers/get-order.query-handler.ts
+++ src/order/queries/handlers/get-order.query-handler.ts
@@ -1,32 +1,21 @@
-import { Inject } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
-import { DynamoDB } from "aws-sdk";
-import { OrderViewModel } from "src/order/schemas/order-view.schema"; // 수정된 import
+import { InjectModel, Model } from "nestjs-dynamoose";
+import { OrderView } from "src/order/repositories/order.repository";
 import { GetOrderQuery } from "../get-order.query";
 
 @QueryHandler(GetOrderQuery)
 export class GetOrderQueryHandler implements IQueryHandler<GetOrderQuery> {
-    private readonly dynamoDB: DynamoDB.DocumentClient;
 
     constructor(
-        @Inject(DynamoDB) private readonly dynamoDBClient: DynamoDB.DocumentClient
-    ) {
-        this.dynamoDB = dynamoDBClient;
-    }
+        @InjectModel("OrderView")
+        private readonly orderViewModel: Model<OrderView, {}>
+    ) {}
 
-    async execute(query: GetOrderQuery): Promise<typeof OrderViewModel[]> {
-        const params = {
-            TableName: "OrderView", // DynamoDB의 테이블 이름
-            FilterExpression: "userId = :userId", // userId 필터
-            ExpressionAttributeValues: {
-                ":userId": query.userId, // 쿼리에서 받은 userId
-            },
-        };
+    async execute(query: GetOrderQuery): Promise<OrderView[]> {
+        const result = await this.orderViewModel.query("userId")
+            .eq(query.userId)
+            .exec();
 
-        // 모든 주문을 스캔
-        const result = await this.dynamoDB.scan(params).promise();
-
-        // 반환된 아이템을 OrderViewModel 타입으로 변환하여 반환
-        return (result.Items || []) as typeof OrderViewModel[];
+        return result;
     }
 }
