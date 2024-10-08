77fc76c2a5971d78584d0dc60976d09875b746a6
seraphicblue <ggj05037@gmail.com>
2024-10-09 04:52:25 +0900

    refactor: about product distance calculate version 3

--- src/product/repositories/product.repository.ts
+++ src/product/repositories/product.repository.ts
@@ -2,9 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { DeepPartial, Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Product } from "../entities/product.entity";
+import { Category } from "../product.category";
 
 @Injectable()
 export class ProductRepository {
+  
   findOneBy: any;
   findByStoreId(stellerId: any) {
     throw new Error("Method not implemented.");
@@ -20,6 +22,42 @@ export class ProductRepository {
     @InjectRepository(Product)
     private readonly repository: Repository<Product>,
   ) {}
+  
+  // private async fetchProductsFromPostgres(category: Category): Promise<Product[]> {
+  //   if (category === Category.ALL) {
+  //     return this.repository.createQueryBuilder('product')
+  //       .leftJoinAndSelect('product.inventory', 'inventory')
+  //       .where('inventory.quantity > :minQuantity', { minQuantity: 0 })
+  //       .orderBy('product.updatedAt', 'DESC')
+  //       .take(100)
+  //       .getMany();
+  //   } else {
+  //     return this.findByCategory(category);
+  //   }
+  // }
+
+  // async findByCategory(category: Category): Promise<Product[]> {
+  //   return this.createQueryBuilder('product')
+  //     .leftJoinAndSelect('product.inventory', 'inventory')
+  //     .where('product.category = :category', { category })
+  //     .andWhere('inventory.quantity > :minQuantity', { minQuantity: 0 })
+  //     .orderBy('product.discountrate', 'DESC')
+  //     .getMany();
+  // }
+
+  // async findByStoreId(sellerId: string): Promise<Product[]> {
+  //   return this.createQueryBuilder('product')
+  //     .leftJoinAndSelect('product.inventory', 'inventory')
+  //     .where('product.sellerId = :sellerId', { sellerId })
+  //     .andWhere('inventory.quantity > :minQuantity', { minQuantity: 0 })
+  //     .orderBy('product.updatedAt', 'DESC')
+  //     .getMany();
+  // }
+
+  createQueryBuilder(alias: string) {
+    return this.repository.createQueryBuilder(alias);
+  }
+
 
   async createProduct({
     sellerId,
