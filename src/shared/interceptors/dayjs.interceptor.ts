b9063223916042e86c4acc869a72df38c193a4c2
seraphicblue <ggj05037@gmail.com>
2024-10-19 03:14:49 +0900

    refactor:rtransformdates

--- src/shared/interceptors/dayjs.interceptor.ts
+++ src/shared/interceptors/dayjs.interceptor.ts
@@ -1,12 +1,10 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-} from "@nestjs/common";
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone"; // timezone 플러그인 import
+
+dayjs.extend(timezone); // timezone 플러그인 사용
 
 @Injectable()
 export class DateTransformInterceptor implements NestInterceptor {
@@ -15,30 +13,19 @@ export class DateTransformInterceptor implements NestInterceptor {
   }
 
   private transformDates(data: any): any {
-    // 데이터가 객체인지 확인하고 storeAddress 필드를 건너뛰도록 설정
-    if (typeof data === 'object' && data !== null) {
-      // storeAddress가 있을 경우 변환을 건너뜀
-      if ('storeAddress' in data) {
-        console.log('Skipping transformation for storeAddress:', data.storeAddress);
-        // storeAddress만 건너뛰고 나머지 필드는 변환
-        Object.keys(data).forEach((key) => {
-          if (key !== 'storeAddress') {
-            data[key] = this.transformDates(data[key]);
-          }
-        });
-        return data; // 변환하지 않은 데이터 반환
-      }
-
-      // 나머지 필드에 대한 처리
-      Object.keys(data).forEach((key) => {
-        data[key] = this.transformDates(data[key]);
-      });
-    } else if (data instanceof Date || (typeof data === "string" && !isNaN(Date.parse(data)))) {
+    if (data instanceof Date || (typeof data === "string" && !isNaN(Date.parse(data)))) {
       return dayjs(data).tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
     } else if (Array.isArray(data)) {
       return data.map((item) => this.transformDates(item));
+    } else if (typeof data === 'object' && data !== null) {
+      const transformedData = { ...data };
+      Object.keys(transformedData).forEach((key) => {
+        if (key !== 'storeAddress') {
+          transformedData[key] = this.transformDates(transformedData[key]);
+        }
+      });
+      return transformedData;
     }
-
     return data;
   }
 }
